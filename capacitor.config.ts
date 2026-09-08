6e0e6aca16a5c87c4c2701d4359054b5f4c799f6
xiu070806 <hn2082229@gmail.com>
2026-09-08 15:16:28 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ev.taximetpro',
+  appName: 'TAXIMET PRO',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
