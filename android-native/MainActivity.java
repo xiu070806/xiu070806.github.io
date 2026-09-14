459103c3002b91c9138fdb4bd0a5040230c55287
xiu070806 <hn2082229@gmail.com>
2026-09-15 01:59:14 +0700

    Add files via upload

--- android-native/MainActivity.java
+++ android-native/MainActivity.java
@@ -1,6 +1,12 @@
 package com.taximet.pro;
 
 import android.os.Bundle;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
